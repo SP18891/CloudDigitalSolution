7fa508f2234dfeacb4475fad139c8be9ef0f1d5b
Shrinivas Pachapule <shrinivas.pachapule@innroad.com>
2025-02-05 12:56:29 +0530

    feat:Added registration screen

---
+++ CloudDigitalSolution/UI/UserRegistration.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace CloudDigitalSolution.UI;
+
+/// <summary>
+/// Interaction logic for UserRegistration.xaml
+/// </summary>
+public partial class UserRegistration : Window
+{
+    public UserRegistration()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        FirstName.Text = string.Empty;
+        LastName.Text = string.Empty;
+        EmailAddress.Text = string.Empty;
+        Gender.Text = string.Empty;
+        MobileNumber.Text = string.Empty;
+        Password.Text = string.Empty;
+        ConfirmPassword.Text = string.Empty;
+    }
+}
